e8c6d4efdae4c22bc82e3413adefd04a64973d34
BudzioT <Budzio.zsm1@gmail.com>
2024-01-06 16:57:42 +0100

    Added background

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,54 @@
 ﻿#include <iostream>
 
-#include "assist.h"
+#include "headers/assist.h"
 
 
 /* Windowe dimensions */
 const int WINDOW_WIDTH = 640;
 const int WINDOW_HEIGHT = 480;
 
+/* Global window */
+SDL_Window* window = nullptr;
+/* Global renderer */
+SDL_Renderer* renderer = nullptr;
+
 
 int main(int argc, char* args[])
 {
-	SDL_Window* window = nullptr;
-	initialize(window);
+	/* Initialize the SDL */
+	if (!initialize()) {
+		printf("Couldn't initialize SDL!");
+		close();
+		return -1;
+	}
 
-	/*
-	SDL_Surface* surf = IMG_Load("images/file.png");
-	if (!surf)
-		printf("Couldn't open images/file.png");
-	*/
+
+	/* Textures */
+	Texture backgroundTexture;
+
+
+	/* Load media */
+	loadMedia(backgroundTexture);
 	
+	/* Close flag */
 	bool quit = false;
+	/* Event variable */
 	SDL_Event event;
 
+	/* Game loop */
 	while (!quit) {
+		/* Poll all events */
 		while (SDL_PollEvent(&event)) {
+			/* If user closed the application set the close flag to true */
 			if (event.type == SDL_QUIT)
 				quit = true;
 		}
+
+		backgroundTexture.render(0, 0);
+		SDL_RenderPresent(renderer);
 	}
 
-	close(window);
+	/* Cleanup */
+	close();
 	return 0;
 }
