80347f971219923ccd8587c407b6535cdad52a5a
BudzioT <Budzio.zsm1@gmail.com>
2024-01-26 22:51:26 +0100

    Game class prototype done, food is disappearing

--- src/Game.cpp
+++ src/Game.cpp
@@ -11,10 +11,19 @@ Game::Game(Texture* snakeTexture, Texture* foodTexture,
 	/* Set map dimensions */
 	m_snake.setMap({ mapX, mapY, mapHeight, mapWidth });
 
+	/* Food dimensions */
+	int foodWidth = m_food.getWidth();
+	int foodHeight = m_food.getHeight();
+
 	/* Change the food position until it's out of the snake's body */
 	do {
-		m_food.changePosition(m_randX(m_gen), m_randY(m_gen));
+		m_food.changePosition(m_randX(m_gen) * foodWidth, m_randY(m_gen) * foodHeight);
 	} while (m_snake.elementCollisionBody(m_food.getPosX(), m_food.getPosY()));
+}
+
+Game::~Game()
+{
+	SDL_RemoveTimer(m_timerID);
 }
 
 void Game::render()
@@ -31,8 +40,68 @@ void Game::start()
 	m_timerID = SDL_AddTimer(m_speed, &Game::snakeMove_callback, this);
 }
 
+void Game::handleEvents(const SDL_Event& event)
+{
+	/* If event is of key down type */
+	if (event.type == SDL_KEYDOWN) {
+		/* Change the direction depending on the key */
+		switch (event.key.keysym.sym) {
+			/* Go up, if snake isn't going down */
+		case SDLK_UP:
+			if (m_currentDirection != Snake_direction::DOWN)
+				m_currentDirection = Snake_direction::UP;
+			break;
+
+			/* Go down, if snake isn't going up */
+		case SDLK_DOWN:
+			if (m_currentDirection != Snake_direction::UP)
+				m_currentDirection = Snake_direction::DOWN;
+			break;
+
+			/* Go left, if snake isn't going right */
+		case SDLK_LEFT:
+			if (m_currentDirection != Snake_direction::RIGHT)
+				m_currentDirection = Snake_direction::LEFT;
+			break;
+
+			/* Go right, if snake isn't going left */
+		case SDLK_RIGHT:
+			if (m_currentDirection != Snake_direction::LEFT)
+				m_currentDirection = Snake_direction::RIGHT;
+			break;
+		}
+	}
+}
+
+void Game::process()
+{
+	/* Now snake position, later food position */
+	int posX = m_snake.getPosX();
+	int posY = m_snake.getPosY();
+
+	/* Food dimensions */
+	int foodWidth = m_food.getWidth();
+	int foodHeight = m_food.getHeight();
+
+	/* If the snakes head touches the food */
+	if ((posX == m_food.getPosX()) && (posY == m_food.getPosY())) {
+		/* Set food position to random until it is out of the snakes body */
+		do {
+			posX = m_randX(m_gen) * foodWidth;
+			posY = m_randY(m_gen) * foodHeight;
+		} while (m_snake.elementCollisionBody(posX, posY));
+
+		/* Change the position */
+		m_food.changePosition(posX, posY);
+		
+		/* Grow the snake */
+		m_snake.grow();
+	}
+}
+
 void Game::changeSpeed(Uint32 speed)
 {
+	/* Change the speed */
 	m_speed = speed;
 }
 
--- src/headers/Game.h
+++ src/headers/Game.h
@@ -8,15 +8,23 @@
 /* Main game logic, it manages all the game mechanics, puts it in one, full whole */
 class Game
 {
+public:
 	Game(Texture* snakeTexture, Texture* foodTexture, const SDL_Rect snakeClips[3],
 		int blockWidth = 0, int blockHeight = 0,
 		int mapHeight = 0, int mapWidth = 0, int mapX = 0, int mapY = 0, int snakeX = 0, int snakeY = 0);
+	~Game();
 
 	/* Render */
 	void render();
 
 	/* Start */
 	void start();
+
+	/* Handle events */
+	void handleEvents(const SDL_Event& event);
+
+	/* Game process */
+	void process();
 
 	/* Change the snake speed, it moves one block per given amount of miliseconds */
 	void changeSpeed(Uint32 speed);
